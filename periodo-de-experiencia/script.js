23f259d9d1cf8f388a063565b440b26e35812db4
contabilfarma <suporte02@contabilfarma.com.br>
2024-12-17 09:34:21 -0300

    Add favicon
    Adicionado uma imagem para o favicon do site

--- periodo-de-experiencia/script.js
+++ periodo-de-experiencia/script.js
@@ -1,19 +1,17 @@
 const data = new Date()
 const hora = data.getHours()
 
-const nome = prompt('Digite seu nome:')
-
 const secao = document.querySelector('.boas-vindas')
  if(hora >= 6 && hora < 12){
-    secao.innerHTML = '<span> Olá, ' + nome +' :) <br> bom dia!!</span>'
+    secao.innerHTML = '<span> Olá, <br> bom dia :)</span>'
     secao.style.color = '#fff'
  } else if(hora >= 12 && hora < 18){
-    secao.innerHTML = '<span> Olá, ' + nome +' :) <br> boa tarde!!</span>'
+    secao.innerHTML = '<span> Olá, <br> boa tarde :)</span>'
     secao.style.color = '#fff'
  } else if(hora >= 18 && hora < 23){
-    secao.innerHTML = '<span> Olá, ' + nome +' :) <br> boa noite!!</span>'
+    secao.innerHTML = '<span> Olá, <br> boa noite :)</span>'
     secao.style.color = '#fff'
  } else{ 
-    secao.innerHTML = '<span> Olá, ' + nome +' :) <br> boa madrugada!!</span>'
+    secao.innerHTML = '<span> Olá, <br> boa madrugada :)</span>'
     secao.style.color = '#fff'
  }
