26c801f4e3d0bdae23d2779e61f5137e8b932b16
contabilfarma <suporte02@contabilfarma.com.br>
2024-12-17 08:02:52 -0300

    Update script.js

--- periodo-de-experiencia/script.js
+++ periodo-de-experiencia/script.js
@@ -5,15 +5,15 @@ const nome = prompt('Digite seu nome')
 
 const secao = document.querySelector('.boas-vindas')
  if(hora >= 6 && hora < 12){
-    secao.innerHTML = '<span> Olá, bom dia!!</span>'
+    secao.innerHTML = '<span> Olá, ' + nome +' :) <br> bom dia!!</span>'
     secao.style.color = '#fff'
  } else if(hora >= 12 && hora < 18){
     secao.innerHTML = '<span> Olá, ' + nome +' :) <br> boa tarde!!</span>'
     secao.style.color = '#fff'
  } else if(hora >= 18 && hora < 23){
-    secao.innerHTML = '<span> Olá, boa noite!!</span>'
+    secao.innerHTML = '<span> Olá, ' + nome +' :) <br> boa noite!!</span>'
     secao.style.color = '#fff'
  } else{ 
-    secao.innerHTML = '<span> Olá, boa madrugada!!</span>'
+    secao.innerHTML = '<span> Olá, ' + nome +' :) <br> boa madrugada!!</span>'
     secao.style.color = '#fff'
  }
